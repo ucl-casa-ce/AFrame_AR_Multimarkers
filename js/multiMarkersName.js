41069536c508c04feced93a7faaebfbd7ad20566
vsigno <v.signorelli@ucl.ac.uk>
2019-12-02 19:01:07 +0000

    First commit

---
+++ js/multiMarkersName.js
@@ -0,0 +1,63 @@
+//Multi Markers AR - Playing the Archive - CASA UCL -Dec2019-
+
+//Global Variable
+var markersURLArray=[];
+var markersNameArray=[];
+
+
+AFRAME.registerComponent('markers_start',{
+	init:function(){
+		console.log('Add markers to the scene');
+
+		var sceneEl = document.querySelector('a-scene');
+		
+		//build the list of the markers_start
+		for(var i=1; i<19; i++)
+		{
+			var url="resources/markers/pattern-Individual_Blocks-"+i+".patt";
+			markersURLArray.push(url);
+			markersNameArray.push('Marker_'+i);
+			//console.log(url);
+		}
+
+		for(var k=0; k<18; k++)
+		{
+			var markerEl = document.createElement('a-marker');
+			markerEl.setAttribute('type','pattern');
+			markerEl.setAttribute('url',markersURLArray[k]);
+			markerEl.setAttribute('id',markersNameArray[k]);
+
+			markerEl.setAttribute('registerevents','');
+			sceneEl.appendChild(markerEl);
+
+			//Adding a dummy text
+			var textEl = document.createElement('a-entity');
+			
+			textEl.setAttribute('id','text');
+			textEl.setAttribute('text',{color: 'red', align: 'center', value:markersNameArray[k], width: '5.5'});
+			
+			textEl.object3D.position.set(0, 0.7, 0);
+			textEl.object3D.rotation.set(-90, 0, 0);
+
+			markerEl.appendChild(textEl);
+		}
+	}
+});
+
+
+//Detect when a marker is detected
+AFRAME.registerComponent('registerevents', {
+		init: function () {
+			const marker = this.el;
+
+			marker.addEventListener("markerFound", ()=> {
+				var markerId = marker.id;
+				console.log('Marker Found: ', markerId);
+			});
+
+			marker.addEventListener("markerLost",() =>{
+				var markerId = marker.id;
+				console.log('Marker Lost: ', markerId);
+			});
+		},
+	});
